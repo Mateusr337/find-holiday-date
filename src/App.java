b93e2e3e6d630447518fbfec90b809f70922ae93
mateus <mateusc.rossetto@hotmail.com>
2023-01-12 23:19:03 -0300

    refactor: interface while logic

--- src/App.java
+++ src/App.java
@@ -6,25 +6,23 @@ public class App {
 
     private static final HandleHolidays handleHolidays = new HandleHolidays();
     private static String command = null;
-
+    
     public static void main(String[] args) throws Exception {
+        
 
         Logger.log("Starting program ...");
         Logger.log("");
+        
 
-
-        while (command != "finish") {
+        while (true) {
             showCommands();
             Logger.log("");
-
+            
             command = Input.scan();
             Logger.log("");
 
             handleCommand(command);
-            Logger.log("");
         }
-
-        Logger.log("Program finished ...");
     }
 
     public static void handleCommand (String command) {
@@ -36,10 +34,10 @@ public class App {
     }
 
     public static void showCommands () {
+        Logger.log("");
         Logger.log("Choose and insert a command ...");
         Logger.log("'date' to search a holiday by date;");
         Logger.log("'name' to search a holiday by name;");
-        Logger.log("'finish' to close application;");
         Logger.log("'all' to search all holidays;");
     }
 }
